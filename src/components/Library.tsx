bd987c9795fe2f13ed89dacb164bc585ed6aa05a
vishnuk7 <vishnu.kraman88@gmail.com>
2020-11-30 15:45:40 +0530

    add responsive style 📱and update readme

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
 import { LibraryContext } from '../contexts/LibraryContext';
 import { SongContext } from '../contexts/SongsContext';
+import { ThemeContext } from '../contexts/ThemeContext';
 import { songInfoType } from '../types/Song.td';
 import LibraryItem from './LibraryItem';
 
 const Library: React.FC = () => {
 	const songsList = useContext(SongContext);
 	const { isLibraryOpen } = useContext(LibraryContext);
+	const { isDark } = useContext(ThemeContext);
 	const songs = songsList.songs as songInfoType[];
 
 	return (
-		<div className={`library ${isLibraryOpen && 'active'}`}>
+		<div className={`library ${isDark && 'is-dark'} ${isLibraryOpen && 'active'}`}>
 			<h2>Library</h2>
 			{songs.map((song) => (
 				<LibraryItem key={song.id} {...song} />
